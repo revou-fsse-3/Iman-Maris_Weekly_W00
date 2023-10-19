f6fbbe0b35a70d355ea0e9f6175bdfadefd56b46
imanmaris99 <imanmaris99@yahoo.com>
2023-10-19 14:00:15 +0700

    modified file README.md index.html script.js style.css

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var myName1;
-let myName2;
-const myName3 = "revou";
+//var myName1;
+//let myName2;
+//const myName3 = "revou";
 
-myName1="Iman";
-myName2="maris";
+//myName1="Iman";
+//myName2="maris";
 
-console.log(myName1, myName2, myName3);
+//console.log(myName1, myName2, myName3);
 
 //Camel case//
+
+function addContact(){
+    alert("Pesan Anda Telah Terkirim..!!!");
+}
+
